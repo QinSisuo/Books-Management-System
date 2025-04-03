d17a049fe8ae63980cd5e6000154ba612e4cb473
IDEA <QinSiSuo@gmail.com>
2025-04-03 17:30:28 +0800

    无用代码注释掉了

--- src/main/java/com/book/service/ReaderInfoService.java
+++ src/main/java/com/book/service/ReaderInfoService.java
@@ -1,44 +1,44 @@
-package com.book.service;
-
-import com.book.mapper.ReaderInfoMapper;
-import com.book.domain.ReaderInfo;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-
-@Service
-public class ReaderInfoService {
-    @Autowired
-    private ReaderInfoMapper readerInfoMapper;
-
+//package com.book.service;
+//
+//import com.book.mapper.ReaderInfoMapper;
+//import com.book.domain.ReaderInfo;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.List;
+//
+//@Service
+//public class ReaderInfoService {
 //    @Autowired
-    public void setReaderInfoDao(ReaderInfoMapper readerInfoMapper) {
-        this.readerInfoMapper = readerInfoMapper;
-    }
-
-    // 获取所有读者信息
-    public List<ReaderInfo> readerInfos() {
-        return readerInfoMapper.getAllReaderInfo();
-    }
-
-    // 删除读者信息
-    public boolean deleteReaderInfo(int readerId) {
-        return readerInfoMapper.deleteReaderInfo(readerId) > 0;
-    }
-
-    // 根据 readerId 获取读者信息
-    public ReaderInfo getReaderInfo(int readerId) {
-        return readerInfoMapper.findReaderInfoByReaderId(readerId);
-    }
-
-    // 编辑读者信息
-    public boolean editReaderInfo(ReaderInfo readerInfo) {
-        return readerInfoMapper.editReaderInfo(readerInfo) > 0;
-    }
-
-    // 添加读者信息
-    public boolean addReaderInfo(ReaderInfo readerInfo) {
-        return readerInfoMapper.addReaderInfo(readerInfo) > 0;
-    }
-}
+//    private ReaderInfoMapper readerInfoMapper;
+//
+////    @Autowired
+//    public void setReaderInfoDao(ReaderInfoMapper readerInfoMapper) {
+//        this.readerInfoMapper = readerInfoMapper;
+//    }
+//
+//    // 获取所有读者信息
+//    public List<ReaderInfo> readerInfos() {
+//        return readerInfoMapper.getAllReaderInfo();
+//    }
+//
+//    // 删除读者信息
+//    public boolean deleteReaderInfo(int readerId) {
+//        return readerInfoMapper.deleteReaderInfo(readerId) > 0;
+//    }
+//
+//    // 根据 readerId 获取读者信息
+//    public ReaderInfo getReaderInfo(int readerId) {
+//        return readerInfoMapper.findReaderInfoByReaderId(readerId);
+//    }
+//
+//    // 编辑读者信息
+//    public boolean editReaderInfo(ReaderInfo readerInfo) {
+//        return readerInfoMapper.editReaderInfo(readerInfo) > 0;
+//    }
+//
+//    // 添加读者信息
+//    public boolean addReaderInfo(ReaderInfo readerInfo) {
+//        return readerInfoMapper.addReaderInfo(readerInfo) > 0;
+//    }
+//}
